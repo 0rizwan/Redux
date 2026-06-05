b4bb45d1e0d8582df6dc9b43b3ace2bc0de01088
0rizwan <fycsrizwan@gmail.com>
2026-06-05 17:55:19 +0530

    RTK Query done using CRUD operations on db.json

--- redux-toolkit/src/api/adminSlice.js
+++ redux-toolkit/src/api/adminSlice.js
@@ -3,11 +3,36 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const adminApi = createApi({
     reducerPath: 'admin',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/' }),
-    endpoints: (build) => ({
-        getUserAccount: build.query({
-            query: () => 'accounts/'
+    endpoints: (builder) => ({
+        getUserAccount: builder.query({
+            query: () => 'accounts',
+            transformResponse: (response) => response.sort((a, b) => b.amount - a.amount),
+            providesTags: ['accounts']
+        }),
+        addAccount: builder.mutation({
+            query: ({ id, amount }) => ({
+                url: 'accounts',
+                method: 'POST',
+                body: { id, amount },
+            }),
+            invalidatesTags: ['accounts']
+        }),
+        deleteAccount: builder.mutation({
+            query: (id) => ({
+                url: `accounts/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['accounts']
+        }),
+        updateAccount: builder.mutation({
+            query: ({ id, amount }) => ({
+                url: `accounts/${id}`,
+                method: 'PATCH',
+                body: { amount }
+            }),
+            invalidatesTags: ['accounts']
         })
     })
-})
+});
 
-export const { useGetUserAccountQuery } = adminApi;
+export const { useGetUserAccountQuery, useAddAccountMutation, useDeleteAccountMutation, useUpdateAccountMutation } = adminApi;
--- redux-toolkit/src/components/Admin.jsx
+++ redux-toolkit/src/components/Admin.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { useGetUserAccountQuery } from '../api/adminSlice'
+import React, { useState } from 'react'
+import { useAddAccountMutation, useGetUserAccountQuery, useDeleteAccountMutation, useUpdateAccountMutation } from '../api/adminSlice'
 
 function Admin() {
     const { data, error, isLoading } = useGetUserAccountQuery();
+    const [addAccount, response] = useAddAccountMutation();
+    const [deleteAccount] = useDeleteAccountMutation();
+    const [updateAccount] = useUpdateAccountMutation();
+    const randomNum = Math.floor(Math.random() * 100 + 10);
 
     return (
         <div style={{ border: '1px solid black', padding: '20px' }}>
             <div>Admin Component</div>
             {
-                data && data.map(account => <div key={account.id}>{account.id} : ${account.amount} </div>)
+                data && data.map(account => <div key={account.id}>
+                    {account.id} : ${account.amount}
+                    <button onClick={() => deleteAccount(account.id)} >Delete account</button>
+                    <button onClick={() => updateAccount({ id: account.id, amount: randomNum })} >Update account</button>
+                </div>)
             }
+            <button onClick={() => addAccount({ id: data.length + 1, amount: randomNum })} >Add account+</button>
         </div>
     )
 }
